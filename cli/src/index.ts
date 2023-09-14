45544780d1064b4853264629801485254eebbbc0
Tchoupinax <corentinfiloche@hotmail.fr>
2023-09-14 11:18:58 +0200

    feat: create the help-menu (#11)
    * feat: create the help-menu
    
    * feat: upgrade dependencies
    
    * feat(docs): set exemples in the CLI

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -22,6 +22,33 @@ const remoteURL = process.env.NODE_ENV === "development" ? "http://localhost:300
 
 async function run() {
   const args = getArguments();
+
+  // Display options
+  if (args.values.help) {
+    console.log(`\nhelm-viewer
+
+  -b/--browser:       allow to open with a specific browser (firefox, chromium)
+  -h/--help:          displays this menu
+  -k/--encryptionKey: specify the encryption key
+  -p/--push:          push the result of the build online and returns an URL
+  -v/--values:        provide values files for the chart, can be provided several times
+  -w/--watch:         activate the watch mode
+
+  # When you are in a chart helm folder
+  helm-viewer
+  
+  # To target a specific path
+  helm-viewer path/of/the/chart
+  
+  # To compute the chart with an external values file
+  helm-viewer path/of/the/chart --values path/of/the/values/file1 --values path/of/the/values/file2
+  
+  # To get a public link (with encrypted data)
+  helm-viewer path/of/the/chart --push
+    `)
+    process.exit(0);
+  }
+
   const currentPath = args.positionals.at(0) ?? process.cwd();
   const valuesPathArray = args.values.values;
 
