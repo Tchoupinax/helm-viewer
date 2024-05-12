2702ff31cd4ca46270b94eafb6dd5fa6a0258b19
Tchoupinax <corentinfiloche@hotmail.fr>
2024-05-12 21:15:29 +0200

    feat: improve UI

---
+++ webapp/components/sidebar-template.vue
@@ -0,0 +1,43 @@
+<template>
+  <div @click="showFiles = !showFiles" class="flex items-center text-2xl cursor-pointer hover:underline font-thin">
+    <p>+ {{ name }}</p>
+    <span class="text-xs font-bold ml-2 rounded-xl bg-cyan-100 p-1 px-2">{{ Object.keys(template).length }}</span>
+  </div>
+
+  <div
+    v-if="showFiles"
+    v-for="file of Object.keys(template)"
+  >
+    <p 
+      class="ml-6 mt-2 flex cursor-pointer pl-2 hover:text-yellow-500"
+      @click="$emit('fileSelected', file)"
+    >
+      {{ file }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export type Store = {
+  showFiles: boolean;
+};
+
+export default {
+  emits: ["fileSelected"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    template: {
+      type: Object,
+      required: true,
+    }
+  },
+  data(): Store {
+    return {
+      showFiles: false,
+    }
+  },
+}
+</script>
---
+++ webapp/components/sidebar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="w-1/3 xl:w-3/12 h-screen overflow-scroll">
+    <h1 class="text-3xl text-center mb-4 mt-2">
+      {{ data.name }}
+    </h1>
+
+    <div class="p-2">
+      <div
+        v-if="data.templated"
+        v-for="template of Object.keys(data.templated).sort((a, b) => a > b)"
+        class="mb-2"
+      >
+        <SidebarTemplate
+          :name="template"
+          :template="data.templated[template]"
+          @file-selected="file => onFileSelected(template, file)"
+        />
+      </div>
+    </div>
+
+    <div v-if="data.sources" class="border-t-2 border-black">
+      <h2 class="italic text-3xl font-thin mt-2 ml-4 mb-4 underline">Sources</h2>
+      <div class="mb-1" v-for="file of Object.keys(data.sources).filter(n => n !== 'templates')">
+        <p
+          class="ml-6 font-thin hover:bg-slate-300 pl-2 cursor-pointer"
+          @click="displaySourceFile(file)"
+        >
+          {{ file }}
+        </p>
+      </div>
+      
+      <p v-if="data.sources['templates']" class="font-thin ml-2">
+        ➡ templates
+      </p>
+
+      <div
+        v-if="data.sources['templates']"
+        v-for="file of Object.keys(data.sources['templates'])"
+        class="mb-1 hover:bg-slate-300 pl-2 cursor-pointer"
+      >
+        <p
+          class="ml-8 font-thin"
+          @click="displaySourceFile(file, true)"
+        >
+          {{ file }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  emits: ["displayTemplateFile"],
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+    fetchDataError: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  methods: {
+    onFileSelected(k8sResourceName: string, file: string) {
+      this.$emit('displayTemplateFile', { file, k8sResourceName })
+    }
+  }
+}
+</script>
