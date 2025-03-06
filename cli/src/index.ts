a070f68cf852283af11cb953fea76a30cc88b355
Tchoupinax <corentinfiloche@hotmail.fr>
2025-03-06 11:06:10 +0100

    fix build

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,4 +1,5 @@
-import { readFileSync } from "node:fs";
+#!/usr/bin/env node
+"use strict";
 
 import chalk from "chalk";
 import { randomUUID } from "crypto";
@@ -7,8 +8,6 @@ import yaml from "js-yaml";
 import { nanoid } from "nanoid";
 import open from "open";
 import { tmpdir } from "os";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 import { checkNodeVersion } from "./functions/check-node-version";
 import { computeChart } from "./functions/compute-chart";
@@ -28,7 +27,7 @@ async function run() {
   checkNodeVersion();
 
   const args = getArguments();
-  const version = JSON.parse(readFileSync("package.json", "utf8")).version;
+  const version = "0.21.3";
 
   // Display options
   if (args.values.help) {
@@ -65,8 +64,8 @@ async function run() {
     valuesPathArray.length > 0
       ? chalk.greenBright(`🔑 Values detected : ${valuesPathArray.join(",")}`)
       : chalk.yellowBright(
-          `⚠️  No value detected, computing with default values in the Chart`,
-        ),
+          `⚠️  No value detected, computing with default values in the Chart`
+        )
   );
 
   const tmpDir = `${tmpdir()}/${randomUUID()}`;
@@ -86,12 +85,12 @@ async function run() {
     payload = await computeChart(
       currentPath,
       args.values.name,
-      valuesPathArray,
+      valuesPathArray
     );
   } catch (err) {
     console.log("\n");
     console.log(
-      chalk.bgRedBright("The computation of the chart failed. (message below)"),
+      chalk.bgRedBright("The computation of the chart failed. (message below)")
     );
     console.log((err as Error).message);
     console.log("\n");
@@ -114,7 +113,7 @@ async function run() {
       currentPath,
       args.values.watch,
       browser,
-      args.values.name,
+      args.values.name
     );
   }
 
@@ -123,7 +122,7 @@ async function run() {
 
 async function pushOnlineFunction(
   payload: { name: string; version: string; templated: any; sources: any },
-  encryptionKey: string,
+  encryptionKey: string
 ) {
   const id = nanoid();
   const { version, name } = yaml.load(payload.sources["Chart.yaml"]) as {
@@ -153,7 +152,7 @@ async function pushOnlineFunction(
 
   fs.writeFileSync(
     ".helm-viewer-url",
-    `${remoteReadURL}?id=${id}&k=${encryptionKey}&o=t`,
+    `${remoteReadURL}?id=${id}&k=${encryptionKey}&o=t`
   );
 }
 
@@ -162,7 +161,7 @@ async function serveLocally(
   currentPath: string,
   watchingMode: boolean,
   browserName: BrowserName,
-  releaseName: string,
+  releaseName: string
 ) {
   const id = nanoid();
   if (process.env.NODE_ENV === "development") {
