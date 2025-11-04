41115fb8f9db5139aa19aac5d1aca3f405ec03cf
Tchoupinax <corentinfiloche@hotmail.fr>
2025-11-04 15:40:24 +0100

    fix: add lines

--- webapp/server/api/chart-upload.ts
+++ webapp/server/api/chart-upload.ts
@@ -4,7 +4,9 @@ import { uploadFileToS3 } from "../infrastructure/upload-to-s3";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
+
   await uploadFileToS3(body.chartId, body.content);
+
   console.log("Chart uploaded!");
 
   return {
